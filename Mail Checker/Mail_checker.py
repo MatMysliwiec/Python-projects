4ae1755b7d7b899b7b385e8bb5d66c37bd28616d
Mateusz Myśliwiec <mati33m@gmail.com>
2024-03-03 22:47:35 +0100

    Add Mail Checker program

---
+++ Mail Checker/Mail_checker.py
@@ -0,0 +1,39 @@
+import imaplib
+import getpass
+import time
+
+
+def check_email_imap(username, password, server, port=993):
+    try:
+        mail = imaplib.IMAP4_SSL(server, port)
+        mail.login(username, password)
+        mail.select("inbox")
+        result, data = mail.search(None, "UNSEEN")
+
+        if result == "OK":
+            email_ids = data[0].split()
+            if email_ids:
+                print(f"You have {len(email_ids)} unread messages")
+                for email_id in email_ids:
+                    result, message_date = mail.fetch(email_id, "(RFC822)")
+                    if result == "OK":
+                        print(f"Email {email_id}:\n{message_date[0][1]}")
+        mail.logout()
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    username = input("Enter your email address: ")
+    password = getpass.getpass("Enter your email password: ")
+    server = input("Enter your email server (e.g. imap.gmail.com): ")
+    port = int(input("Enter the server port (press Enter for default port = 993): "))
+
+    interval = int(input("Enter the interval for checking emails in seconds: "))
+
+    try:
+        while True:
+            check_email_imap(username, password, server, port)
+            time.sleep(interval)
+    except KeyboardInterrupt:
+        print("Mail checker stopped")
